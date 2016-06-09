6a0fd5ab15e6254d64b597587392886ffff930d9
hanjia2014 <hanj@datacom.co.nz>
2016-06-10 11:53:21 +1200

    add tsconfig

---
+++ OrientXpress/Scripts/site/ng2/app/app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ox-app',
+    templateUrl: `<h1>{{1+1}}</h1>`
+})
+export class AppComponent {
+    
+    public constructor() {
+        
+    }
+}
