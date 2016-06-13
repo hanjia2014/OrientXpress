f6cbf27e630d643d0977dd9c930cfbe137d176e3
hanjia2014 <hanj@datacom.co.nz>
2016-06-13 14:55:34 +1200

    add config.js

--- OrientXpress/ox-app/ox.app.component.ts
+++ OrientXpress/ox-app/ox.app.component.ts
@@ -1,11 +1,11 @@
 ﻿import { Component } from '@angular/core';
 import {Event} from "./event";
-import { EventService } from './event.service';
+//import { EventService } from './event.service';
 
 @Component({
     selector: 'ox-app',
     templateUrl: `ox-app/event-summary.html`,
-    providers: [EventService]
+    //providers: [EventService]
 })
 export class AppComponent {
     events: Event[];
@@ -14,12 +14,12 @@ export class AppComponent {
         this.selectedEvent = event;
     }
     getEvents = () => {
-        this.events = this.eventService.getEvents();
+        //this.events = this.eventService.getEvents();
     }
     ngOnInit() {
         this.getEvents();
     }
-    public constructor(private eventService: EventService) {
+    public constructor() {
         
     }
 }
