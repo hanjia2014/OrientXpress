343117eb28714872ed186488ab44c87703b9ae02
hanjia2014 <hanj@datacom.co.nz>
2016-06-15 09:06:36 +1200

    remove unused code

--- OrientXpress/ox-app/event.service.ts
+++ OrientXpress/ox-app/event.service.ts
@@ -7,7 +7,6 @@ export class EventService {
     eventsUrl: string = 'http://localhost:30712/api/events';
     private events: Event[];
     constructor(private http: Http) {
-        this.getEvents();
     }
     getEvents = () => {
         this.events = this.http.get(this.eventsUrl).map((res: any) => res.json()).map((rawEvents: any) => rawEvents.map(Event.create));
