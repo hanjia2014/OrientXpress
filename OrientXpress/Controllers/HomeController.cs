e2e32730ba5088966aeba4aff17c6267fde13205
hanjia2014 <hanj@datacom.co.nz>
2016-06-16 17:10:52 +1200

    add events

--- OrientXpress/Controllers/HomeController.cs
+++ OrientXpress/Controllers/HomeController.cs
@@ -1,9 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using OrientXpress.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace OrientXpress.Controllers
 {
     public class HomeController : Controller
     {
+        private OXDataContext db = new OXDataContext();
+
+        public List<Event> Events
+        {
+            get
+            {
+                var events = new List<Event>();
+                foreach(var eve in db.Events)
+                {
+                    events.Add(new Event { Id = eve.Id, Content = eve.Content, Date = eve.Date, Title = eve.Title });
+                }
+
+                return events;
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
