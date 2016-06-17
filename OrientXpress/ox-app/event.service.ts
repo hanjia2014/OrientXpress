05409e711b432e0a2819c4d631578d0508ed267d
hanjia2014 <hanj@datacom.co.nz>
2016-06-17 13:37:47 +1200

    removed old code

--- OrientXpress/ox-app/event.service.ts
+++ OrientXpress/ox-app/event.service.ts
@@ -7,32 +7,19 @@ import { Event } from './event';
 export class EventService {
     eventsUrl: string = 'api/events';
     private events: Event[];
+    private error: any;
     constructor(private http: Http) {
     }
     getEvents(): Observable<Event[]> {
 
         this.http.get(this.eventsUrl).map((res: Response) => {
             if (res.status != 200) {
-                throw new Error('No comments to retrieve! code status ' + res.status);
+                throw new Error('No objects to retrieve! code status ' + res.status);
             } else {
                 return res.json();
             }
         }).subscribe(
             (data: Event[]) => { return data; },
-            (err) => this.error = err);
-    }
-
-    private extractData(res: Response) {
-        let body = res.json();
-        return body.data || {};
-    }
-
-    private handleError(error: any) {
-        // In a real world app, we might use a remote logging infrastructure
-        // We'd also dig deeper into the error to get a better message
-        let errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-        console.error(errMsg); // log to console instead
-        return Observable.throw(errMsg);
+            (err: any) => this.error = err);
     }
 }
