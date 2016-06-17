480e407019d6c6f30d89bc8f1f53f7f8892366cf
hanjia2014 <hanj@datacom.co.nz>
2016-06-17 12:39:41 +1200

    add json type

---
+++ OrientXpress/Controllers/ListController.cs
@@ -0,0 +1,37 @@
+﻿using OrientXpress.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace OrientXpress.Controllers
+{
+    public class ListController : ApiController
+    {
+        private OXDataContext db = new OXDataContext();
+        // GET api/<controller>
+        public IQueryable<string> Get()
+        {
+            return db.Events.Select(p => p.Title);
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
