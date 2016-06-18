e6c5028a078a4135a85541d29d7a2500fe6b127a
hanjia2014 <hanj@datacom.co.nz>
2016-06-18 13:01:00 +1200

    add error handler

--- OrientXpress/ox-app/main.ts
+++ OrientXpress/ox-app/main.ts
@@ -2,4 +2,7 @@
 import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent }   from './ox.app.component';
 
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+bootstrap(AppComponent, [HTTP_PROVIDERS]).then(
+    (success: any) => console.log('app bootstrapped...'),
+    (error: any) => console.log(error)
+);
