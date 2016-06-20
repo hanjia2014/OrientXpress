18e4fcb93dc19f4e78202e8d1b1722e51e034c98
hanjia2014 <hanj@datacom.co.nz>
2016-06-20 16:21:21 +1200

    add musician admin pages

---
+++ OrientXpress/Areas/Admin/Controllers/MusiciansController.cs
@@ -0,0 +1,134 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using OrientXpress.Models;
+using System.Web;
+using System.IO;
+
+namespace OrientXpress.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class MusiciansController : Controller
+    {
+        private OXDataContext db = new OXDataContext();
+
+        // GET: Admin/Musicians
+        public ActionResult Index()
+        {
+            return View(db.Musicians.ToList());
+        }
+
+        // GET: Admin/Musicians/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musician musician = db.Musicians.Find(id);
+            if (musician == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musician);
+        }
+
+        // GET: Admin/Musicians/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Musicians/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,FirstName,LastName,Description,Image")] Musician musician)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Musicians.Add(musician);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(musician);
+        }
+
+        // GET: Admin/Musicians/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musician musician = db.Musicians.Find(id);
+            if (musician == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musician);
+        }
+
+        // POST: Admin/Musicians/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Musician musician, HttpPostedFileBase imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageFile != null && imageFile.ContentLength > 0)
+                {
+                    var target = new MemoryStream();
+                    imageFile.InputStream.CopyTo(target);
+                    byte[] data = target.ToArray();
+                    musician.Image = data;
+                }
+
+                db.Entry(musician).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(musician);
+        }
+
+        // GET: Admin/Musicians/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musician musician = db.Musicians.Find(id);
+            if (musician == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musician);
+        }
+
+        // POST: Admin/Musicians/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Musician musician = db.Musicians.Find(id);
+            db.Musicians.Remove(musician);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- OrientXpress/Models/Musician.cs
+++ OrientXpress/Models/Musician.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OrientXpress.Models
@@ -14,5 +15,11 @@ namespace OrientXpress.Models
         public string LastName { get; set; }
         public string Description { get; set; }
         public byte[] Image { get; set; }
+
+        public string ConvertImageString()
+        {
+            var base64 = Convert.ToBase64String(Image);
+            return string.Format("data:image/gif;base64,{0}", base64);
+        }
     }
 }
