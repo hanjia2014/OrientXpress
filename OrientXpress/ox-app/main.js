56dcb9959d62dd20ac13039ba8cc83f0d54c8672
Han Jia <hanj@datacom.co.nz>
2016-07-12 10:26:43 +1200

    update typescript build

--- OrientXpress/ox-app/main.js
+++ OrientXpress/ox-app/main.js
@@ -3,6 +3,5 @@ var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var http_1 = require('@angular/http');
 var router_1 = require('@angular/router');
 var ox_app_component_1 = require('./ox.app.component');
-var ox_app_routes_1 = require('./ox.app.routes');
-platform_browser_dynamic_1.bootstrap(ox_app_component_1.AppComponent, [http_1.HTTP_PROVIDERS, router_1.ROUTER_PROVIDERS, ox_app_routes_1.APP_ROUTER_PROVIDERS]).then(function (success) { return console.log('app bootstrapped...'); }, function (error) { return console.log(error); });
+platform_browser_dynamic_1.bootstrap(ox_app_component_1.AppComponent, [http_1.HTTP_PROVIDERS, router_1.ROUTER_PROVIDERS]).then(function (success) { return console.log('app bootstrapped...'); }, function (error) { return console.log(error); });
 //# sourceMappingURL=main.js.map
